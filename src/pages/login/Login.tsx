9ce048ea7280f67328744d5fcc75bf40265ce3cb
Ebert Escalante <ebertescalant@gmail.com>
2025-12-02 11:03:43 -0500

    feat: improve error handling in login and register forms, add detailed error messages for user feedback

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -25,6 +25,7 @@ import { httpStatusCodes, paths } from "../../core/constants";
 import Auth from "../../core/services/auth/auth";
 import {
 	dispatchNotifyStack,
+	dispatchNotifyStackError,
 } from "../../core/services/notistack";
 import {
 	useGoogleLoginMutation,
@@ -35,27 +36,27 @@ import useStyles from "./Login.styles";
 import * as userConstants from "./model/LoginConstants";
 
 const schema = Yup.object().shape({
-  [userConstants.USER_NAME]: Yup.string().required(
-    localize("common.fieldRequired")
-  ),
-  [userConstants.PASSWORD]: Yup.string().required(
-    localize("common.fieldRequired")
-  ),
+	[userConstants.USER_NAME]: Yup.string().required(
+		localize("common.fieldRequired")
+	),
+	[userConstants.PASSWORD]: Yup.string().required(
+		localize("common.fieldRequired")
+	),
 });
 
 const Login: React.FC = () => {
 	const navigate = useNavigate();
-  const styles = useStyles();
-  const resolver = useYupValidationResolver(schema);
+	const styles = useStyles();
+	const resolver = useYupValidationResolver(schema);
 	const [showPassword, setShowPassword] = useState(false);
 
-  const [login, { isLoading }] = useLoginMutation();
+	const [login, { isLoading }] = useLoginMutation();
 	const [googleLogin] = useGoogleLoginMutation();
 
-  const { handleSubmit, control, formState: { errors } } = useForm<{ email: string; password: string }>({
+	const { handleSubmit, control, formState: { errors } } = useForm<{ email: string; password: string }>({
 		defaultValues: { email: '', password: '' },
-    resolver,
-  });
+		resolver,
+	});
 
 	/* const loginGoogle = useGoogleLogin({
 		onSuccess: (credentialResponse) => {
@@ -77,22 +78,28 @@ const Login: React.FC = () => {
 		}
 	}); */
 
-  const handleSubmitForm = (data: { email: string; password: string }) => {
-    Auth.logout();
-    login(data)
-      .unwrap()
-      .then((res) => {
-        Auth.setUserToken(res.data.token);
+	const handleSubmitForm = (data: { email: string; password: string }) => {
+		Auth.logout();
+		login(data)
+			.unwrap()
+			.then((res) => {
+				Auth.setUserToken(res.data.token);
 				Auth.setUserInfo(res.data.user);
 				navigate(paths.DASHBOARD);
-      })
-      .catch(() => {
-        dispatchNotifyStack({ message: localize("login.error") }, httpStatusCodes.BAD_REQUEST);
-      });
-  };
+			})
+			.catch(res => {
+				if (Array.isArray(res?.data?.messages) && res?.data?.messages.length > 0) {
+					res?.data?.messages.forEach((msg: { message: string }) => {
+						dispatchNotifyStackError(msg?.message)
+					})
+				} else {
+					dispatchNotifyStack({ message: localize("login.error") }, httpStatusCodes.BAD_REQUEST);
+				}
+			});
+	};
 
-  const handleShowPassword = () => setShowPassword((show) => !show);
-  const handleRegister = () => navigate(paths.REGISTER);
+	const handleShowPassword = () => setShowPassword((show) => !show);
+	const handleRegister = () => navigate(paths.REGISTER);
 
 	const handleProviderSignIn = (token: string | undefined) => {
 		if (!token) return;
@@ -103,46 +110,52 @@ const Login: React.FC = () => {
 				Auth.setUserInfo(res.data.user);
 				navigate(paths.DASHBOARD);
 			})
-			.catch(() => {
-				dispatchNotifyStack({ message: localize("login.error") }, httpStatusCodes.BAD_REQUEST);
+			.catch(res => {
+				if (Array.isArray(res?.data?.messages) && res?.data?.messages.length > 0) {
+					res?.data?.messages.forEach((msg: { message: string }) => {
+						dispatchNotifyStackError(msg?.message)
+					})
+				} else {
+					dispatchNotifyStack({ message: localize("login.error") }, httpStatusCodes.BAD_REQUEST);
+				}
 			});
 	};
 
-  return (
-    <Box>
-      <Grid container sx={{ height: "100vh", overflow: "hidden" }}>
-        <Grid item md={6} sx={{ display: { xs: "none", md: "block" } }}>
-          <img
-            src={Apartment}
-            alt="apartment"
-            className={styles.classes.image}
-          />
-        </Grid>
-        <Grid item md={6} xs={12} sx={{ paddingX: { xs: "20px" } }}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              height: "100%",
-              maxHeight: "calc(100vh - 0px)",
-              overflowY: "auto",
-            }}
-          >
-            <img
-              src={logo}
-              alt="logo"
-              className={`${styles.classes.logo}`}
-            />
-            <br />
-            <Typography
-              variant="h5"
-              sx={{ fontWeight: "bold", textAlign: "center" }}
-            >
+	return (
+		<Box>
+			<Grid container sx={{ height: "100vh", overflow: "hidden" }}>
+				<Grid item md={6} sx={{ display: { xs: "none", md: "block" } }}>
+					<img
+						src={Apartment}
+						alt="apartment"
+						className={styles.classes.image}
+					/>
+				</Grid>
+				<Grid item md={6} xs={12} sx={{ paddingX: { xs: "20px" } }}>
+					<Box
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+							alignItems: "center",
+							height: "100%",
+							maxHeight: "calc(100vh - 0px)",
+							overflowY: "auto",
+						}}
+					>
+						<img
+							src={logo}
+							alt="logo"
+							className={`${styles.classes.logo}`}
+						/>
+						<br />
+						<Typography
+							variant="h5"
+							sx={{ fontWeight: "bold", textAlign: "center" }}
+						>
 							Inicia sesión en tu cuenta
-            </Typography>
-            <Box sx={{ width: "100%", maxWidth: "400px" }}>
+						</Typography>
+						<Box sx={{ width: "100%", maxWidth: "400px" }}>
 							<Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
 								{/* <Button
 									variant="outlined"
@@ -179,99 +192,99 @@ const Login: React.FC = () => {
 									- o -
 								</Typography>
 							</Box>
-              <form onSubmit={handleSubmit(handleSubmitForm)}>
-                <CustomInput
-                  id={userConstants.USER_NAME}
-                  control={control}
-                  label={localize("login.userName")}
-                  placeholder={localize("login.userPlaceholder")}
-                  errorText={errors[userConstants.USER_NAME]?.message}
-                  error={!!errors[userConstants.USER_NAME]}
-                />
+							<form onSubmit={handleSubmit(handleSubmitForm)}>
+								<CustomInput
+									id={userConstants.USER_NAME}
+									control={control}
+									label={localize("login.userName")}
+									placeholder={localize("login.userPlaceholder")}
+									errorText={errors[userConstants.USER_NAME]?.message}
+									error={!!errors[userConstants.USER_NAME]}
+								/>
 
-                <Controller
-                  control={control}
-                  name={userConstants.PASSWORD}
-                  rules={{
-                    required: {
-                      value: true,
-                      message: localize("common.fieldRequired"),
-                    },
-                  }}
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      type={showPassword ? "text" : "password"}
-                      id={userConstants.PASSWORD}
-                      label={localize("login.password")}
-                      variant="outlined"
-                      placeholder={localize("login.passPlaceholder")}
-                      error={!!errors[userConstants.PASSWORD]}
-                      helperText={errors[userConstants.PASSWORD]?.message}
-                      margin="normal"
-                      fullWidth
-                      InputProps={{
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <IconButton onClick={handleShowPassword} edge="end">
-                              {showPassword ? (
-                                <VisibilityIcon />
-                              ) : (
-                                <VisibilityOffIcon />
-                              )}
-                            </IconButton>
-                          </InputAdornment>
-                        ),
-                      }}
-                    />
-                  )}
-                />
-                <Button
-                  type="submit"
-                  variant="contained"
-                  fullWidth
-                  size="large"
-                  startIcon={
-                    isLoading ? (
-                      <CircularProgress size={20} color="inherit" />
-                    ) : null
-                  }
-                  disabled={isLoading}
-                  sx={{ mt: 1, textTransform: "uppercase" }}
-                >
-                  Iniciar sesión
-                </Button>
-              </form>
-              <br />
-              <Divider />
-              <Grid container justifyContent="center" gap={2} marginTop={2}>
-                <Grid item>
-                  <Typography variant="subtitle1">
-                    {"¿No tienes una cuenta?"}
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Typography variant="subtitle1" gutterBottom>
-                    <Link
-                      onClick={handleRegister}
-                      color="inherit"
-                      underline="none"
-                      fontWeight="bold"
-                      sx={{
-                        textDecoration: "none",
-                        cursor: "pointer",
-                        "&:hover": {
-                          color: "secondary",
-                          textDecoration: "underline",
-                        },
-                      }}
-                    >
-                      Registrarse
-                    </Link>
-                  </Typography>
-                </Grid>
-              </Grid>
-              {/* <Grid container justifyContent="center">
+								<Controller
+									control={control}
+									name={userConstants.PASSWORD}
+									rules={{
+										required: {
+											value: true,
+											message: localize("common.fieldRequired"),
+										},
+									}}
+									render={({ field }) => (
+										<TextField
+											{...field}
+											type={showPassword ? "text" : "password"}
+											id={userConstants.PASSWORD}
+											label={localize("login.password")}
+											variant="outlined"
+											placeholder={localize("login.passPlaceholder")}
+											error={!!errors[userConstants.PASSWORD]}
+											helperText={errors[userConstants.PASSWORD]?.message}
+											margin="normal"
+											fullWidth
+											InputProps={{
+												endAdornment: (
+													<InputAdornment position="end">
+														<IconButton onClick={handleShowPassword} edge="end">
+															{showPassword ? (
+																<VisibilityIcon />
+															) : (
+																<VisibilityOffIcon />
+															)}
+														</IconButton>
+													</InputAdornment>
+												),
+											}}
+										/>
+									)}
+								/>
+								<Button
+									type="submit"
+									variant="contained"
+									fullWidth
+									size="large"
+									startIcon={
+										isLoading ? (
+											<CircularProgress size={20} color="inherit" />
+										) : null
+									}
+									disabled={isLoading}
+									sx={{ mt: 1, textTransform: "uppercase" }}
+								>
+									Iniciar sesión
+								</Button>
+							</form>
+							<br />
+							<Divider />
+							<Grid container justifyContent="center" gap={2} marginTop={2}>
+								<Grid item>
+									<Typography variant="subtitle1">
+										{"¿No tienes una cuenta?"}
+									</Typography>
+								</Grid>
+								<Grid item>
+									<Typography variant="subtitle1" gutterBottom>
+										<Link
+											onClick={handleRegister}
+											color="inherit"
+											underline="none"
+											fontWeight="bold"
+											sx={{
+												textDecoration: "none",
+												cursor: "pointer",
+												"&:hover": {
+													color: "secondary",
+													textDecoration: "underline",
+												},
+											}}
+										>
+											Registrarse
+										</Link>
+									</Typography>
+								</Grid>
+							</Grid>
+							{/* <Grid container justifyContent="center">
                 <Typography variant="subtitle2" gutterBottom>
                   <Link
                     onClick={handleForgotPassword}
@@ -291,11 +304,11 @@ const Login: React.FC = () => {
                   </Link>
                 </Typography>
               </Grid> */}
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+						</Box>
+					</Box>
+				</Grid>
+			</Grid>
+		</Box>
+	);
 };
 export default Login;
--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -5,15 +5,15 @@ import useYupValidationResolver from "@/core/hooks/useYupValidationResolver";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import {
-  Box,
-  Button,
-  Divider,
-  Grid,
-  IconButton,
-  InputAdornment,
-  Link,
-  TextField,
-  Typography,
+	Box,
+	Button,
+	Divider,
+	Grid,
+	IconButton,
+	InputAdornment,
+	Link,
+	TextField,
+	Typography,
 } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
 import React, { useState } from "react";
@@ -22,7 +22,7 @@ import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 import { httpStatusCodes, paths } from "../../core/constants";
 import Auth from "../../core/services/auth/auth";
-import { dispatchNotifyStack } from "../../core/services/notistack";
+import { dispatchNotifyStack, dispatchNotifyStackError } from "../../core/services/notistack";
 import { useRegisterMutation } from "../../stateManagement/apiSlices/userApi";
 import localize from "../../utils/localizer";
 import useStyles from "./register.styles";
@@ -30,139 +30,145 @@ import * as userConstants from "./model/registerConstants";
 import { RegisterRequestDto } from "@/stateManagement/models/auth/register";
 
 const schema = Yup.object().shape({
-  [userConstants.USER_NAME]: Yup.string().required(localize("common.fieldRequired")),
-  [userConstants.EMAIL]: Yup.string()
-    .email(localize("common.invalidEmail"))
-    .required(localize("common.fieldRequired")),
-  [userConstants.PASSWORD]: Yup.string().required(localize("common.fieldRequired")),
+	[userConstants.USER_NAME]: Yup.string().required(localize("common.fieldRequired")),
+	[userConstants.EMAIL]: Yup.string()
+		.email(localize("common.invalidEmail"))
+		.required(localize("common.fieldRequired")),
+	[userConstants.PASSWORD]: Yup.string().required(localize("common.fieldRequired")),
 	[userConstants.CONFIRM_PASSWORD]: Yup.string()
 		.oneOf([Yup.ref(userConstants.PASSWORD)], localize("register.passwordsMustMatch"))
 		.required(localize("common.fieldRequired")),
 });
 
 const Register: React.FC = () => {
-  const navigate = useNavigate();
-  const styles = useStyles();
-  const resolver = useYupValidationResolver(schema);
-  const [showPassword, setShowPassword] = useState(false);
+	const navigate = useNavigate();
+	const styles = useStyles();
+	const resolver = useYupValidationResolver(schema);
+	const [showPassword, setShowPassword] = useState(false);
 	const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-  const [register, { isLoading }] = useRegisterMutation();
+	const [register, { isLoading }] = useRegisterMutation();
 
-  const { handleSubmit, control, formState: { errors } } = useForm<RegisterRequestDto & { confirmPassword: string }>({
-    defaultValues: { name: '', email: '', password: '', confirmPassword: '' },
-    resolver,
-  });
+	const { handleSubmit, control, formState: { errors } } = useForm<RegisterRequestDto & { confirmPassword: string }>({
+		defaultValues: { name: '', email: '', password: '', confirmPassword: '' },
+		resolver,
+	});
 
-  const handleSubmitForm = (data: RegisterRequestDto & { confirmPassword: string }) => {
+	const handleSubmitForm = (data: RegisterRequestDto & { confirmPassword: string }) => {
 		const { confirmPassword, ...rest } = data;
 
-    register({ ...rest })
-      .unwrap()
-      .then((res) => {
-        Auth.setUserToken(res.data.token);
-        Auth.setUserInfo(res.data.user);
-        navigate(paths.DASHBOARD);
-      })
-      .catch(() => {
-        dispatchNotifyStack({ message: localize("register.error") }, httpStatusCodes.BAD_REQUEST);
-      });
-  };
+		register({ ...rest })
+			.unwrap()
+			.then((res) => {
+				Auth.setUserToken(res.data.token);
+				Auth.setUserInfo(res.data.user);
+				navigate(paths.DASHBOARD);
+			})
+			.catch(res => {
+				if (Array.isArray(res?.data?.messages) && res?.data?.messages.length > 0) {
+					res?.data?.messages.forEach((msg: { message: string }) => {
+						dispatchNotifyStackError(msg?.message)
+					})
+				} else {
+					dispatchNotifyStack({ message: localize("register.error") }, httpStatusCodes.BAD_REQUEST);
+				}
+			});
+	};
 
-  const handleShowPassword = () => setShowPassword((show) => !show);
-  const handleLogin = () => navigate(paths.LOGIN);
+	const handleShowPassword = () => setShowPassword((show) => !show);
+	const handleLogin = () => navigate(paths.LOGIN);
 
-  return (
-    <Box>
-      <Grid container sx={{ height: "100vh", overflow: "hidden" }}>
-        <Grid item md={6} sx={{ display: { xs: "none", md: "block" } }}>
-          <img
-            src={Apartment}
-            alt="apartment"
-            className={styles.classes.image}
-          />
-        </Grid>
-        <Grid item md={6} xs={12} sx={{ paddingX: { xs: "20px" } }}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              height: "100%",
-              maxHeight: "calc(100vh - 0px)",
-              overflowY: "auto",
-            }}
-          >
-            <img
-              src={logo}
-              alt="logo"
-              className={`${styles.classes.logo}`}
-            />
-            <br />
-            <Typography
-              variant="h5"
-              sx={{ fontWeight: "bold", textAlign: "center" }}
-            >
-              Crea tu cuenta
-            </Typography>
-            <Box sx={{ width: "100%", maxWidth: "400px" }}>
-              <form onSubmit={handleSubmit(handleSubmitForm)}>
-                <CustomInput
-                  id={userConstants.USER_NAME}
-                  control={control}
-                  label={localize("register.userName")}
-                  placeholder={localize("register.userNamePlaceholder")}
-                  errorText={errors[userConstants.USER_NAME]?.message}
-                  error={!!errors[userConstants.USER_NAME]}
-                />
+	return (
+		<Box>
+			<Grid container sx={{ height: "100vh", overflow: "hidden" }}>
+				<Grid item md={6} sx={{ display: { xs: "none", md: "block" } }}>
+					<img
+						src={Apartment}
+						alt="apartment"
+						className={styles.classes.image}
+					/>
+				</Grid>
+				<Grid item md={6} xs={12} sx={{ paddingX: { xs: "20px" } }}>
+					<Box
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+							alignItems: "center",
+							height: "100%",
+							maxHeight: "calc(100vh - 0px)",
+							overflowY: "auto",
+						}}
+					>
+						<img
+							src={logo}
+							alt="logo"
+							className={`${styles.classes.logo}`}
+						/>
+						<br />
+						<Typography
+							variant="h5"
+							sx={{ fontWeight: "bold", textAlign: "center" }}
+						>
+							Crea tu cuenta
+						</Typography>
+						<Box sx={{ width: "100%", maxWidth: "400px" }}>
+							<form onSubmit={handleSubmit(handleSubmitForm)}>
+								<CustomInput
+									id={userConstants.USER_NAME}
+									control={control}
+									label={localize("register.userName")}
+									placeholder={localize("register.userNamePlaceholder")}
+									errorText={errors[userConstants.USER_NAME]?.message}
+									error={!!errors[userConstants.USER_NAME]}
+								/>
 
-                <CustomInput
-                  id={userConstants.EMAIL}
-                  control={control}
-                  label={localize("register.email")}
-                  placeholder={localize("register.emailPlaceholder")}
-                  errorText={errors[userConstants.EMAIL]?.message}
-                  error={!!errors[userConstants.EMAIL]}
-                />
+								<CustomInput
+									id={userConstants.EMAIL}
+									control={control}
+									label={localize("register.email")}
+									placeholder={localize("register.emailPlaceholder")}
+									errorText={errors[userConstants.EMAIL]?.message}
+									error={!!errors[userConstants.EMAIL]}
+								/>
 
-                <Controller
-                  control={control}
-                  name={userConstants.PASSWORD}
-                  rules={{
-                    required: {
-                      value: true,
-                      message: localize("common.fieldRequired"),
-                    },
-                  }}
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      type={showPassword ? "text" : "password"}
-                      id={userConstants.PASSWORD}
-                      label={localize("register.password")}
-                      variant="outlined"
-                      placeholder={localize("register.passwordPlaceholder")}
-                      error={!!errors[userConstants.PASSWORD]}
-                      helperText={errors[userConstants.PASSWORD]?.message}
-                      margin="normal"
-                      fullWidth
-                      InputProps={{
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <IconButton onClick={handleShowPassword} edge="end">
-                              {showPassword ? (
-                                <VisibilityIcon />
-                              ) : (
-                                <VisibilityOffIcon />
-                              )}
-                            </IconButton>
-                          </InputAdornment>
-                        ),
-                      }}
-                    />
-                  )}
-                />
+								<Controller
+									control={control}
+									name={userConstants.PASSWORD}
+									rules={{
+										required: {
+											value: true,
+											message: localize("common.fieldRequired"),
+										},
+									}}
+									render={({ field }) => (
+										<TextField
+											{...field}
+											type={showPassword ? "text" : "password"}
+											id={userConstants.PASSWORD}
+											label={localize("register.password")}
+											variant="outlined"
+											placeholder={localize("register.passwordPlaceholder")}
+											error={!!errors[userConstants.PASSWORD]}
+											helperText={errors[userConstants.PASSWORD]?.message}
+											margin="normal"
+											fullWidth
+											InputProps={{
+												endAdornment: (
+													<InputAdornment position="end">
+														<IconButton onClick={handleShowPassword} edge="end">
+															{showPassword ? (
+																<VisibilityIcon />
+															) : (
+																<VisibilityOffIcon />
+															)}
+														</IconButton>
+													</InputAdornment>
+												),
+											}}
+										/>
+									)}
+								/>
 								{/* confirm password */}
 								<Controller
 									control={control}
@@ -201,57 +207,57 @@ const Register: React.FC = () => {
 										/>
 									)}
 								/>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  fullWidth
-                  size="large"
-                  startIcon={
-                    isLoading ? (
-                      <CircularProgress size={20} color="inherit" />
-                    ) : null
-                  }
-                  disabled={isLoading}
-                  sx={{ mt: 1, textTransform: "uppercase" }}
-                >
-                  Registrarse
-                </Button>
-              </form>
-              <br />
-              <Divider />
-              <Grid container justifyContent="center" gap={2} marginTop={2}>
-                <Grid item>
-                  <Typography variant="subtitle1">
-                    {"¿Ya tienes una cuenta?"}
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Typography variant="subtitle1" gutterBottom>
-                    <Link
-                      onClick={handleLogin}
-                      color="inherit"
-                      underline="none"
-                      fontWeight="bold"
-                      sx={{
-                        textDecoration: "none",
-                        cursor: "pointer",
-                        "&:hover": {
-                          color: "secondary",
-                          textDecoration: "underline",
-                        },
-                      }}
-                    >
-                      Inicia sesión
-                    </Link>
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+								<Button
+									type="submit"
+									variant="contained"
+									fullWidth
+									size="large"
+									startIcon={
+										isLoading ? (
+											<CircularProgress size={20} color="inherit" />
+										) : null
+									}
+									disabled={isLoading}
+									sx={{ mt: 1, textTransform: "uppercase" }}
+								>
+									Registrarse
+								</Button>
+							</form>
+							<br />
+							<Divider />
+							<Grid container justifyContent="center" gap={2} marginTop={2}>
+								<Grid item>
+									<Typography variant="subtitle1">
+										{"¿Ya tienes una cuenta?"}
+									</Typography>
+								</Grid>
+								<Grid item>
+									<Typography variant="subtitle1" gutterBottom>
+										<Link
+											onClick={handleLogin}
+											color="inherit"
+											underline="none"
+											fontWeight="bold"
+											sx={{
+												textDecoration: "none",
+												cursor: "pointer",
+												"&:hover": {
+													color: "secondary",
+													textDecoration: "underline",
+												},
+											}}
+										>
+											Inicia sesión
+										</Link>
+									</Typography>
+								</Grid>
+							</Grid>
+						</Box>
+					</Box>
+				</Grid>
+			</Grid>
+		</Box>
+	);
 };
 
 export default Register;
